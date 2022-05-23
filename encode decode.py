a946bef0ceb292ff44599d2d3ff567c3b6b2339f
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-05-23 07:44:23 +0000

    Update encode decode.py

--- encode decode.py
+++ encode decode.py
@@ -1,4 +1,6 @@
-#base64 decode
-a= ""
+
+a= "wfefwef"
 print("Encode = ",a.encode("base64"))
 print("Decode = ",a.decode("base64"))
+decode = '灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸彥ㄴㅡて㝽'
+print(decode.encode('utf-16-be'))
