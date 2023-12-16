0eed2f82a21da34ff4085d94c1eb3a8c6642252a
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-12-16 09:40:35 +0530

    Create sum of digits in a number.py

---
+++ Ravindrababu Ravula/sum of digits in a number.py
@@ -0,0 +1,7 @@
+# sum of digits in a number 00:01
+two_digit_number = input("Enter a two digits number : ")
+print(type(two_digit_number))
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+result = int(first_digit) + int(second_digit)
+print(result)
