d10dc18997ef81a5245d5993733f43d527acf4bd
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-12-16 09:26:56 +0530

    Create Primitive_Data_Types.py

---
+++ Ravindrababu Ravula/Primitive_Data_Types.py
@@ -0,0 +1,20 @@
+# Primitive Data Types
+# String 00:19
+print("Ravindra"[0])
+print("Ravindra"[4])
+print("Ravindra"[-1])
+print("Ravindra"[-4])
+
+print("123" + "345")
+
+# _ in number 03:45
+number = 123_4567_689
+print(number)
+
+# Floating point number 04:28 
+num = 12.5
+print(num)
+
+# boolean 04:47
+boo = True
+print(boo)
