7f7b401a7e87cec4404e406b92bbe0825de351ac
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-12-16 08:53:19 +0530

    Create String_Manipulation.py

---
+++ Ravindrababu Ravula/String_Manipulation.py
@@ -0,0 +1,20 @@
+print("Hello World Hello World Hello World ")
+
+# Method 01
+print("\nMethod 01 output")
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+# Method 02
+print("\nMethod 02 output")
+print("Hello World \nHello World \nHello World ")
+
+
+# String Concatenation 
+print("\nString Concatenation Output")
+print("Hello" + "RBR")
+print("Hello " + "RBR")
+print("Hello" + " RBR")
+print("Hello" + " " + "RBR")
