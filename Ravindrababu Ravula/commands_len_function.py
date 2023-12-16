e14dcacff9b8d407a71947e89102c88550a93635
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-12-16 09:04:16 +0530

    Create commands_len_function.py

---
+++ Ravindrababu Ravula/commands_len_function.py
@@ -0,0 +1,15 @@
+# Single Line Commands 00:01
+# This is a python course
+# Advanced Level
+# from basics 
+
+# Multi line Commands 01:02
+"""
+This is a python course
+Advanced Level
+from basics 
+"""
+
+# print how many character in a string 1:40
+print(len(input("What is yout name ")))
+print(len("Ravi"))
