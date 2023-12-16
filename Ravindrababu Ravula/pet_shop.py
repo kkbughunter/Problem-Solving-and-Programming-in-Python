3e4e20854628fbec0b16edf66be2e8e172d6803b
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-12-16 09:20:16 +0530

    Create pet_shop.py

---
+++ Ravindrababu Ravula/pet_shop.py
@@ -0,0 +1,5 @@
+# pet shop 00:08
+print("--Welcome to pet shop name genrator----")
+city = input("Which city did you grow up?\n")
+pet = input("What is your Pet name ?\n")
+print("Your pet shop name is " + city + " " + pet)
